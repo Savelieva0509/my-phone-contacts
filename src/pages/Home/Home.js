e9e3886bc7190bdd053d63df11980cf55aee60da
Babenko Liudmyla <savelieva0509@gmail.com>
2023-01-24 18:00:25 +0200

    стилизация

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,12 @@
+import css from './Home.module.css'; 
 
 export default function Home() {
   return (
-    <div>
-      <h1>
-        Task manager welcome page{' '}
-        <span role="img" aria-label="Greeting icon">
-          💁‍♀️
-        </span>
+    <div className={css.wrapperHomePage}>
+      <h1 className={css.homeTitle}>
+        Hello, this is your PhoneBook. It is designed to help your brain not
+        remember numbers, but write them down. Create your own unique account
+        and get the opportunity to record your contacts.
       </h1>
     </div>
   );
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,7 +1,8 @@
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { register } from 'redux/auth/authOperations';
-import css from './Register.module.css';
+// import css from './Register.module.css';
+import { Button, TextField, Box, Container, Typography } from '@mui/material';
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
@@ -30,33 +31,77 @@ const RegisterForm = () => {
     setPassword('');
     };
     
-  return (
-    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.label}>
-        Username
-         <input type="text" name="name" value={name} onChange={handleChange} />
-      </label>
-      <label className={css.label}>
-        Email
-         <input
-            type="email"
+ return (
+    <Container component="div" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          backgroundColor:  'white',
+          padding: '20px',
+          boxShadow: ' rgb(33 33 33) 0px 2px 10px 1px',
+        }}
+      >
+        <Typography
+          variant="h4"
+          style={{ fontFamily: 'Caveat', fontWeight: '600' }}
+        >
+          Sign Up
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="User name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+            value={name}
+            variant="standard"
+            onChange={handleChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Email Address"
             name="email"
+            autoComplete="email"
+            type="email"
             value={email}
-            onChange={handleChange}
+            variant="standard"
+           onChange={handleChange}
+          
           />
-      </label>
-      <label className={css.label}>
-        Password
-        <input
-            type="password"
+          <TextField
+            margin="normal"
+            required
+            fullWidth
             name="password"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
             value={password}
+            variant="standard"
             onChange={handleChange}
           />
-      </label>
-      <button type="submit">Register</button>
-    </form>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, color: '#0f1111', background: 'd4abca' }}
+          >
+            Sign Up
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
-};
+}
+
 
 export default RegisterForm;
